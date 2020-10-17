07dad0db932cdc0f3b4b49cb513772aa9226f5fb
MidwestMister <timothymichaelkemp@gmail.com>
2020-10-17 08:35:24 -0500

    color blocks now work correctly

--- assets/script.js
+++ assets/script.js
@@ -7,42 +7,70 @@ $(document).ready(function () {
 
     $('#currentDay').text(todayText)
 
+    var hoursPerDay = 9;
+    var time12 = [];
+    var time24 = [];
+    var currentHour = moment().format('H');
 
-    var timeBlockList = [
-        "9 AM",
-        "10 AM",
-        "11 AM",
-        "12 PM",
-        "1 PM",
-        "2 PM",
-        "3 PM",
-        "4 PM",
-        "5 PM"
-    ];
+    function timeEl() {
+        var formatTime12;
+        var formatTime24;
 
-    for (var i = 0; i < timeBlockList.length; i++) {
+        for (i = 9; i < hoursPerDay + 9; i++) {
+
+            formatTime12 = (moment()
+                .startOf("day")
+                .add(i, "hours"))
+                .format("h A");
+
+            formatTime24 = (moment()
+                .startOf("day")
+                .add(i, "hours"))
+                .format("HH");
+
+            time12.push(formatTime12);
+            time24.push(formatTime24);
+        }
+    }
+
+    timeEl();
 
 
-        // 2. Create a variable named "letterBtn" equal to $("<button>");
+    for (var i = 0; i < time12.length; i++) {
+
         var timeBlock = $("<div>");
         var hour = $("<div>");
         var textArea = $("<textarea>");
         var saveButton = $("<button>");
         var icon = $("<li>");
 
-        // 3. Then give each "letterBtn" the following classes: "letter-button" "letter" "letter-button-color".
-        timeBlock.addClass("row time-block");
-        hour.addClass("col-2 hour").attr("style", "padding-top: 10px");
-        textArea.addClass("col-8");
-        saveButton.addClass("col-2 saveBtn");
-        icon.html('<i class="far fa-save fa-lg"></i>').attr("style", "list-style-type:none");
+        if (currentHour === time24[i]) {
+            textArea.addClass("present");
+        }
 
+        if (currentHour > time24[i]) {
+            textArea.addClass("past");
+        }
 
-        // 4. Then give each "letterBtn" a data-attribute called "data-letter".
-        timeBlock.attr("time-block", timeBlockList[i]);
+        if (currentHour < time24[i]) {
+            textArea.addClass("future");
+        }
 
-        // 5. Then give each "letterBtns" a text equal to "letters[i]".
-        hour.text(timeBlockList[i]);
+        // 3. Then give each "letterBtn" the following classes: "letter-button"
+        timeBlock
+            .addClass("row time-block")
+            .attr("time-block", time12[i]);
+        hour
+            .addClass("col-2 hour")
+            .attr("style", "padding-top: 2rem")
+            .text(time12[i]);
+        textArea
+            .addClass("col-8");
+        saveButton
+            .addClass("col-2 saveBtn");
+        icon
+            .html('<i class="far fa-save fa-lg"></i>')
+            .attr("style", "list-style-type:none");
 
         // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
         $(timeBlock).append(hour);
