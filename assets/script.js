ffe59b078c214b28a71cde8a4ec32735f44f513c
MidwestMister <timothymichaelkemp@gmail.com>
2020-10-17 10:13:19 -0500

    corrected color of time blocks logic

--- assets/script.js
+++ assets/script.js
@@ -1,127 +1,176 @@
 $(document).ready(function () {
 
-    var todayDayName = moment().format('dddd');
-    var todayMonthName = moment().format('MMMM');
-    var todayDayNum = moment().format('Do');
-    var todayText = todayDayName + ', ' + todayMonthName + ' ' + todayDayNum;
+  var todayDayName = moment().format('dddd');
+  var todayMonthName = moment().format('MMMM');
+  var todayDayNum = moment().format('Do');
+  var todayText = todayDayName + ', ' + todayMonthName + ' ' + todayDayNum;
 
-    $('#currentDay').text(todayText)
+  $('#currentDay').text(todayText)
 
-    var hoursPerDay = 9;
-    var time12 = [];
-    var time24 = [];
-    var currentHour = moment().format('H');
+  var hoursPerDay = 9;
+  var time12 = [];
+  var time24 = [];
+  var currentHour = moment().format('H');
 
-    function timeEl() {
-        var formatTime12;
-        var formatTime24;
+  function timeEl() {
+    var formatTime12;
+    var formatTime24;
 
-        for (i = 9; i < hoursPerDay + 9; i++) {
+    for (i = 9; i < hoursPerDay + 9; i++) {
 
-            formatTime12 = (moment()
-                .startOf("day")
-                .add(i, "hours"))
-                .format("h A");
+      formatTime12 = (moment()
+        .startOf("day")
+        .add(i, "hours"))
+        .format("h A");
 
-            formatTime24 = (moment()
-                .startOf("day")
-                .add(i, "hours"))
-                .format("H");
+      formatTime24 = (moment()
+        .startOf("day")
+        .add(i, "hours"))
+        .format("H");
 
-            time12.push(formatTime12);
-            time24.push(formatTime24);
-        }
+      time12.push(formatTime12);
+      time24.push(formatTime24);
+    }
+  }
+
+  timeEl();
+
+
+  for (var i = 0; i < time12.length; i++) {
+
+    var timeBlock = $("<div>");
+    var hour = $("<div>");
+    var textArea = $("<textarea>");
+    var saveButton = $("<button>");
+    var icon = $("<li>");
+    var timeDiff = currentHour - time24[i]
+
+    console.log('currentHour :>> ', currentHour);
+    console.log('time24[i] :>> ', time24[i]);
+    console.log('timeDiff :>> ', timeDiff);
+
+    if (timeDiff === 0) {
+      textArea.addClass("present");
     }
 
-    timeEl();
-
-
-    for (var i = 0; i < time12.length; i++) {
-
-        var timeBlock = $("<div>");
-        var hour = $("<div>");
-        var textArea = $("<textarea>");
-        var saveButton = $("<button>");
-        var icon = $("<li>");
-
-        console.log('currentHour :>> ', currentHour);
-        console.log('time24[i] :>> ', time24[i]);
-
-        if (currentHour === time24[i]) {
-            textArea.addClass("present");
-        }
-
-        if (currentHour > time24[i]) {
-            textArea.addClass("past");
-        }
-
-        if (currentHour < time24[i]) {
-            textArea.addClass("future");
-        }
-
-        // 3. Then give each "letterBtn" the following classes: "letter-button"
-        timeBlock
-            .addClass("row time-block")
-            .attr("time-block", time12[i]);
-        hour
-            .addClass("col-2 hour")
-            .attr("style", "padding-top: 2rem")
-            .text(time12[i]);
-        textArea
-            .addClass("col-8");
-        saveButton
-            .addClass("col-2 saveBtn")
-            .attr("id", "save-" + parseInt(time12[i]));
-        icon
-            .html('<i class="far fa-save fa-lg"></i>')
-            .attr("style", "list-style-type:none");
-
-        // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
-        $(timeBlock).append(hour);
-        $(timeBlock).append(textArea);
-        $(timeBlock).append(saveButton);
-        $(saveButton).append(icon);
-        $(".container").append(timeBlock);
-
+    if (timeDiff > 0) {
+      textArea.addClass("past");
     }
 
-    /*
-    signUpButton.addEventListener("click", function (event) {
-        event.preventDefault();
+    if (timeDiff < 0) {
+      textArea.addClass("future");
+    }
 
-        // create user object from submission
-        var user = {
-            firstName: firstNameInput.value.trim(),
-            lastName: lastNameInput.value.trim(),
-            email: emailInput.value.trim(),
-            password: passwordInput.value.trim()
-        };
+    timeBlock
+      .addClass("row time-block")
+      .attr("time-block", time12[i]);
+    hour
+      .addClass("col-2 hour")
+      .attr("style", "padding-top: 2rem")
+      .text(time12[i]);
+    textArea
+      .addClass("col-8");
+    saveButton
+      .addClass("col-2 saveBtn")
+      .attr("id", "save-" + parseInt(time12[i]));
+    icon
+      .html('<i class="far fa-save fa-lg"></i>')
+      .attr("style", "list-style-type:none");
 
-        console.log(user);
+    $(timeBlock).append(hour);
+    $(timeBlock).append(textArea);
+    $(timeBlock).append(saveButton);
+    $(saveButton).append(icon);
+    $(".container").append(timeBlock);
 
-        // validate the fields
-        if (user.firstName === "") {
-            displayMessage("error", "First name cannot be blank");
-        } else if (user.lastName === "") {
-            displayMessage("error", "Last name cannot be blank");
-        } else if (user.email === "") {
-            displayMessage("error", "Email cannot be blank");
-        } else if (user.password === "") {
-            displayMessage("error", "Password cannot be blank");
-        } else {
-            displayMessage("success", "Registered successfully");
+  }
 
-            // set new submission
-            localStorage.setItem("user", JSON.stringify(user));
+  /*
+  var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
 
-            // get most recent submission
-            var lastUser = JSON.parse(localStorage.getItem("user"));
-            userFirstNameSpan.textContent = lastUser.firstName;
-            userLastNameSpan.textContent = lastUser.lastName;
-            userEmailSpan.textContent = lastUser.email;
-            userPasswordSpan.textContent = lastUser.password;
-        }
-    });
+var todos = [];
+
+init();
+
+function renderTodos() {
+// Clear todoList element and update todoCountSpan
+todoList.innerHTML = "";
+todoCountSpan.textContent = todos.length;
+
+// Render a new li for each todo
+for (var i = 0; i < todos.length; i++) {
+  var todo = todos[i];
+
+  var li = document.createElement("li");
+  li.textContent = todo;
+  li.setAttribute("data-index", i);
+
+  var button = document.createElement("button");
+  button.textContent = "Complete";
+
+  li.appendChild(button);
+  todoList.appendChild(li);
+}
+}
+
+function init() {
+// Get stored todos from localStorage
+// Parsing the JSON string to an object
+var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+// If todos were retrieved from localStorage, update the todos array to it
+if (storedTodos !== null) {
+  todos = storedTodos;
+}
+
+// Render todos to the DOM
+renderTodos();
+}
+
+function storeTodos() {
+// Stringify and set "todos" key in localStorage to todos array
+localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// When form is submitted...
+todoForm.addEventListener("submit", function(event) {
+event.preventDefault();
+
+var todoText = todoInput.value.trim();
+
+// Return from function early if submitted todoText is blank
+if (todoText === "") {
+  return;
+}
+
+// Add new todoText to todos array, clear the input
+todos.push(todoText);
+todoInput.value = "";
+
+// Store updated todos in localStorage, re-render the list
+storeTodos();
+renderTodos();
+});
+
+// When a element inside of the todoList is clicked...
+todoList.addEventListener("click", function(event) {
+var element = event.target;
+
+// If that element is a button...
+if (element.matches("button") === true) {
+  // Get its data-index value and remove the todo element from the list
+  var index = element.parentElement.getAttribute("data-index");
+  todos.splice(index, 1);
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  renderTodos();
+}
+});
+
 */
 
 
